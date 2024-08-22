af5c5322e6d3c6292ad125cda20be80385efeaff
youssef hamed <yousiefhamed1@gmail.com>
2024-08-22 19:26:51 +0300

    fixing navigation to the post

--- src/app/components/blog/BlogPost.jsx
+++ src/app/components/blog/BlogPost.jsx
@@ -10,7 +10,7 @@ export default function BlogPost({ params }) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/blog/${params.id}`)
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/blogs/${params.id}`)
       .then((response) => response.json())
       .then((data) => {
         setPost(data);
