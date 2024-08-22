4eeef4e82eaccdda7cc6e0331d2790450fab70c9
Youssef Hamed <yousiefhamed@gmail.com>
2024-08-22 13:48:00 +0300

    adding bg-gradient-color component

---
+++ src/app/components/GradientDecor.jsx
@@ -0,0 +1,16 @@
+const GradientDecor = () => {
+  return (
+    <div className=" w-full h-full fixed top-0 left-0 -z-20 overflow-x-hidden">
+      <span
+        style={{ background: "linear-gradient(#fb923cae, #c084fcae)" }}
+        className="absolute w-[500px] h-[500px] top-0 left-0 -translate-x-1/2 -translate-y-1/2 rounded-full blur-3xl"
+      ></span>
+      <span
+        style={{ background: "linear-gradient(#fb923cae, #c084fcae)" }}
+        className="absolute w-[500px] h-[500px] bottom-0 right-0 translate-x-1/2 translate-y-1/2 rounded-full blur-3xl"
+      ></span>
+    </div>
+  );
+};
+
+export default GradientDecor;
