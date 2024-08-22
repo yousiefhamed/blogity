3e06b0e3a86064dbe3e0ef1f1473c2b1a51867f5
Youssef Hamed <yousiefhamed@gmail.com>
2024-08-22 13:51:10 +0300

    adding Navbar component and app layout

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex justify-between items-center bg-zinc-100 sticky top-0 p-4 max-w-screen-lg mx-auto z-10">
+      <div className="absolute w-screen h-full top-0 left-1/2 -translate-x-1/2 shadow-md -z-10 bg-zinc-100"></div>
+      <div className="logo">
+        <Link href="/" className="text-2xl font-bold">
+          <span className="text-purple-800">blog</span>
+          <span className="text-yellow-400">ity</span>
+        </Link>
+      </div>
+      <ul className="flex space-x-4">
+        <li>
+          <Link
+            href="/"
+            className="text-purple-800 hover:text-yellow-400 hover:font-bold font-semibold transition-all"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/blog"
+            className="text-purple-800 hover:text-yellow-400 hover:font-bold font-semibold transition-all"
+          >
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/add"
+            className="text-purple-800 hover:text-yellow-400 hover:font-bold font-semibold transition-all"
+          >
+            Add
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className="text-purple-800 hover:text-yellow-400 hover:font-bold font-semibold transition-all"
+          >
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import GradientDecor from "./components/GradientDecor";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-zinc-100`}>
+        <GradientDecor />
+        <Navbar />
+        <main className="w-full max-w-screen-lg mx-auto">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
