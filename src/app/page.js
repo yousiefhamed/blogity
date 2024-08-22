58ea0e06ae2a1bd2ba50aa7d12470e94f6918f7a
Youssef Hamed <yousiefhamed@gmail.com>
2024-08-22 13:52:05 +0300

    adding app entry point

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="w-full flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <Hero heading="Welcome to Blogity" />
+    </>
   );
 }
